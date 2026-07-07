0957e1a6ba3c7167eca2396d484b8e9f1790c3f2
Fabio Cordeiro <fabiojpcordeiro@outlook.com>
2026-07-07 08:54:51 -0300

    fix: vite config

--- crud-api/src/config/config.php
+++ crud-api/src/config/config.php
@@ -5,4 +5,5 @@ $dataFile = __DIR__ . '/../../data/data.json';
 $allowedOrigins = [
     'http://0.0.0.0:8080', 'http://localhost:8080', 'http://127.0.0.1:8080',
     'http://0.0.0.0:5500', 'http://localhost:5500', 'http://127.0.0.1:5500',
+    'http://0.0.0.0:5173', 'http://localhost:5173', 'http://127.0.0.1:5173'
 ];
